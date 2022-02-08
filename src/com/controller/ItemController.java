e7d9e1b6b1435ae2048430626e07f47185dfecd4
Bisma Bendesa <bismabendesajaya@gmail.com>
2022-02-08 11:14:19 +0800

    Create ItemController.java in com.controller

---
+++ src/com/controller/ItemController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+import com.view.Item;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Bisma
+ */
+public interface ItemController {
+    public void Save(Item item) throws SQLException;
+    public void New(Item item) throws SQLException;
+    public void Edit(Item item) throws SQLException;
+    public void Delete(Item item) throws SQLException;
+}

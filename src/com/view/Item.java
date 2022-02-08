bd9a0cd9d0a1c02c2008cea286382832590d2c8e
Bisma Bendesa <bismabendesajaya@gmail.com>
2022-02-08 11:09:21 +0800

    change Barang.java to Item.java in view package

--- src/com/view/Item.java
+++ src/com/view/Item.java
@@ -11,12 +11,12 @@ package com.view;
  *
  * @author Bisma
  */
-public class Barang extends javax.swing.JFrame {
+public class Item extends javax.swing.JFrame {
 
     /**
      * Creates new form Inventory
      */
-    public Barang() {
+    public Item() {
         initComponents();
     }
 
@@ -249,20 +249,21 @@ public class Barang extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Barang.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Barang.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Barang.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Barang.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Item.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Barang().setVisible(true);
+                new Item().setVisible(true);
             }
         });
     }
